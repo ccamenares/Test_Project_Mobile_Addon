9c7fdc0a59c200352700772f82e4d68ce449d58a
Christopher <camenarescoding@gmail.com>
2017-08-06 17:58:49 -0400

    Find name elements

--- src/main/java/tld/domain/my_mobile_addon/AddPersonAction.java
+++ src/main/java/tld/domain/my_mobile_addon/AddPersonAction.java
@@ -1,5 +1,7 @@
 package tld.domain.my_mobile_addon;
 
+import org.openqa.selenium.By;
+
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.testproject.java.enums.ExecutionResultType;
@@ -7,11 +9,21 @@ import io.testproject.java.sdk.generated.codeblocks.Action;
 
 public class AddPersonAction extends Action {
 
+	String firstName;
+	String lastName;
+	
 	@Override
 	protected ExecutionResultType execute() throws Exception {
 		
 		// Get the driver
 		AndroidDriver<MobileElement> androidDriver = this.getAndroidDriver(MobileElement.class);
+		
+		// Find First name element and type first name (e.g. John)
+		MobileElement firstNameElement = androidDriver.findElement(By.id("firstName"));
+		firstNameElement.sendKeys(this.firstName);
+		// Find Last name element and type last name (e.g. 'Smith')
+		MobileElement lastNameElement = androidDriver.findElement(By.id("lastName"));
+		lastNameElement.sendKeys(this.lastName);
 		
 		return null;
 	}
