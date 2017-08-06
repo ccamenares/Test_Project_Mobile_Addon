3ee09a56b0b51bc0b699b4f84f91fc0514463b36
Christopher <camenarescoding@gmail.com>
2017-08-06 18:48:42 -0400

    Obtain driver settings

--- src/main/java/tld/domain/my_mobile_addon/PersonAddonRunner.java
+++ src/main/java/tld/domain/my_mobile_addon/PersonAddonRunner.java
@@ -1,5 +1,23 @@
 package tld.domain.my_mobile_addon;
 
-public class PersonAddonRunner {
+import io.testproject.java.classes.DriverSettings;
+import io.testproject.java.classes.MobileDriverSettings;
+import io.testproject.java.enums.DriverType;
 
+public class PersonAddonRunner {
+	
+	private static DriverSettings getDriverSettings() {
+		DriverSettings driverSettings = new DriverSettings();
+		// Declare that Android driver is required
+		driverSettings.setDriverType(DriverType.Appium_Android);
+		// Specify target device UDID and Name
+		driverSettings.setDeviceUdid("DEVICE_UDID");
+		driverSettings.setDeviceName("DEVICE_NAME");
+		// Specify mobile app settings
+		MobileDriverSettings mobileSettings = new MobileDriverSettings();
+		mobileSettings.setAndroidPackage("io.testproject.demo");
+		mobileSettings.setAndroidActivity(".MainActivity");
+		driverSettings.setMobileSettings(mobileSettings);
+		return driverSettings;
+	}
 }
