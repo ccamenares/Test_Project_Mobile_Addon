e5b26b70b04c4408cdeb2744e7880b3a7b54381d
Christopher <camenarescoding@gmail.com>
2017-08-06 18:52:17 -0400

    Action methods

--- src/main/java/tld/domain/my_mobile_addon/PersonAddonRunner.java
+++ src/main/java/tld/domain/my_mobile_addon/PersonAddonRunner.java
@@ -6,6 +6,33 @@ import io.testproject.java.enums.DriverType;
 
 public class PersonAddonRunner {
 	
+	private static void runAddPersonCodeBlock(String firstName, String lastName) {
+		try {
+			// Create an instance of the AddPerson action
+			AddPersonAction addPerson = new AddPersonAction();
+			// Set the parameters
+			addPerson.firstName = firstName;
+			addPerson.lastName = lastName;
+			// Run the action
+			runner.run(addPerson);
+		} catch (Throwable e) {
+			// Handle failure...
+		}
+	}
+	
+	private static void runFindPersonCodeBlock(String fullName) {
+		try {
+			// Create an instance of the FindPersonInListAction
+			FindPersonInListAction findPerson = new FindPersonInListAction();
+			// Set the parameters
+			findPerson.fullName = fullName;
+			// Run the action (passing the "persons" element locator)
+			runner.run(findPerson, By.id("persons"));
+		} catch (Throwable e) {
+			// Handle failure...
+		}
+	}
+	
 	private static DriverSettings getDriverSettings() {
 		DriverSettings driverSettings = new DriverSettings();
 		// Declare that Android driver is required
