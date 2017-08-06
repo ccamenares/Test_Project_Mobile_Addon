47de42472d1be0b034188c040c4a52ecd591c067
Christopher <camenarescoding@gmail.com>
2017-08-06 18:14:23 -0400

    Create second class

---
+++ src/main/java/tld/domain/my_mobile_addon/FindPersonInListAction.java
@@ -0,0 +1,14 @@
+package tld.domain.my_mobile_addon;
+
+import io.testproject.java.enums.ExecutionResultType;
+import io.testproject.java.sdk.generated.codeblocks.ElementAction;
+
+public class FindPersonInListAction extends ElementAction {
+
+	@Override
+	protected ExecutionResultType execute() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
