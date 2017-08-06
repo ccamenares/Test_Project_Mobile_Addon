e1a5e979df959b5a5757fdcbfad13f60dc09fbef
Christopher <camenarescoding@gmail.com>
2017-08-06 18:16:32 -0400

    Get element for ElementAction

--- src/main/java/tld/domain/my_mobile_addon/FindPersonInListAction.java
+++ src/main/java/tld/domain/my_mobile_addon/FindPersonInListAction.java
@@ -1,5 +1,6 @@
 package tld.domain.my_mobile_addon;
 
+import io.appium.java_client.MobileElement;
 import io.testproject.java.enums.ExecutionResultType;
 import io.testproject.java.sdk.generated.codeblocks.ElementAction;
 
@@ -7,7 +8,10 @@ public class FindPersonInListAction extends ElementAction {
 
 	@Override
 	protected ExecutionResultType execute() throws Exception {
-		// TODO Auto-generated method stub
+		
+		// Get containing element that was passed by the runner
+		MobileElement personsList = this.getElement(MobileElement.class);
+		
 		return null;
 	}
 
